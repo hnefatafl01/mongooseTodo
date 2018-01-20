a907ebf325279d810ecaa90b70c71649c0bfecca
hnefatafl01 <kevin.erick01@gmail.com>
2018-01-19 17:41:52 -0700

    fix delete token route

--- server/server.js
+++ server/server.js
@@ -145,12 +145,12 @@ app.post('/users/login', async (req, res) => {
 });
 
 app.delete('/users/me/token', authenticate, async (req, res) => {
+
     try {
-        await req.user.removeToken(req.token)
+        const user = await req.user.removeToken(req.token)
         res.status(200).send();
     } catch(e) {
         res.status(400).send();
-
     }
 });
 
